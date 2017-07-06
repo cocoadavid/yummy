2f6898d49a76ce2148c5484826a53a2697cf5a90
David Szilard <szilard.david91@gmail.com>
2017-07-06 17:51:27 +0200

    database connection works, created HTTPHandler and APIHandler

---
+++ src/main/java/com/codecool/yummy/controller/HTTPRequestHandler.java
@@ -0,0 +1,35 @@
+package com.codecool.yummy.controller;
+
+import com.codecool.yummy.database.DBHandler;
+import com.codecool.yummy.database.RecipeORMEntity;
+import com.codecool.yummy.database.RecipeORMRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by szilarddavid on 2017.07.06..
+ */
+@Controller
+public class HTTPRequestHandler {
+
+    private RecipeORMRepository recipeORMRepository;
+
+    @Autowired
+    private DBHandler dbHandler;
+
+    @Autowired
+    HTTPRequestHandler(RecipeORMRepository recipeORMRepository) {
+        this.recipeORMRepository = recipeORMRepository;
+    }
+
+    @RequestMapping("/feed")
+    public String requestThatReturnsTemplate(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+
+        model.addAttribute("name", recipeORMRepository.findRecipeByName("hamburger").get(0).getName());
+        return "feed"; // The name of the template
+    }
+
+}
