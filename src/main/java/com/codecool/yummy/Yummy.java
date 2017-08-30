b92703ce8b805f179ac39f2693ca703dd1ccc683
David Szilard <szilard.david91@gmail.com>
2017-08-30 19:34:12 +0200

    deleted StorageService and unnecessary comments

--- src/main/java/com/codecool/yummy/Yummy.java
+++ src/main/java/com/codecool/yummy/Yummy.java
@@ -1,11 +1,7 @@
 package com.codecool.yummy;
 
-import com.codecool.yummy.service.StorageService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.Resource;
 
 /**
  * Created by szilarddavid on 2017.06.21..
